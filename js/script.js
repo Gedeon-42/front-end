d591acec81b11b1bafec90e8a19001723426ceb1
Gedeon <umwizerwagedeon@gmail.com>
2022-06-25 07:31:45 -0900

    added contact and footer section in index.hhtml file

---
+++ js/script.js
@@ -0,0 +1,5 @@
+console.log("hellowordl capo");
+var date = new Date();
+//console.log(date.getFullYear());
+var output = (document.getElementById("copyright").innerHTML =
+  "copyright" + " " + date.getFullYear());
