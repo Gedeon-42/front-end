dfa0d7d151e9bb5af0906d2a4edea947d79b2feb
Gedeon <umwizerwagedeon@gmail.com>
2022-06-25 16:40:10 -0900

    added fonts and add some style in main.css

--- js/script.js
+++ js/script.js
@@ -3,3 +3,63 @@ var date = new Date();
 //console.log(date.getFullYear());
 var output = (document.getElementById("copyright").innerHTML =
   "copyright" + " " + date.getFullYear());
+let app = Vue.createApp({
+  data: function () {
+    return {
+      message: "hello vue js",
+      typeValue: "",
+      typeStatus: false,
+      displayTextArray: [
+        "YouTuber",
+        " frontend Developer",
+        "Blogger",
+        "ui/ux Designer",
+        "Freelancer",
+      ],
+      typingSpeed: 100,
+      erasingSpeed: 100,
+      newTextDelay: 2000,
+      displayTextArrayIndex: 0,
+      charIndex: 0,
+    };
+  },
+  props: {},
+  created() {
+    setTimeout(this.typeText, this.newTextDelay + 200);
+  },
+  methods: {
+    typeText() {
+      if (
+        this.charIndex <
+        this.displayTextArray[this.displayTextArrayIndex].length
+      ) {
+        if (!this.typeStatus) this.typeStatus = true;
+        this.typeValue += this.displayTextArray[
+          this.displayTextArrayIndex
+        ].charAt(this.charIndex);
+        this.charIndex += 1;
+        setTimeout(this.typeText, this.typingSpeed);
+      } else {
+        this.typeStatus = false;
+        setTimeout(this.eraseText, this.newTextDelay);
+      }
+    },
+    eraseText() {
+      if (this.charIndex > 0) {
+        if (!this.typeStatus) this.typeStatus = true;
+        this.typeValue = this.displayTextArray[
+          this.displayTextArrayIndex
+        ].substring(0, this.charIndex - 1);
+        this.charIndex -= 1;
+        setTimeout(this.eraseText, this.erasingSpeed);
+      } else {
+        this.typeStatus = false;
+        this.displayTextArrayIndex += 1;
+        if (this.displayTextArrayIndex >= this.displayTextArray.length)
+          this.displayTextArrayIndex = 0;
+        setTimeout(this.typeText, this.typingSpeed + 1000);
+      }
+    },
+  },
+});
+app.mount("#root");
